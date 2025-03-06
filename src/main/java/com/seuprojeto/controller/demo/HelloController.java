a4723118f34af2250b74cb323767760b3fd3032d
Gmicheletti <guilherme_micheletti@hotmail.com>
2025-03-06 08:48:05 -0300

    ajuste mensagem

--- src/main/java/com/seuprojeto/controller/demo/HelloController.java
+++ src/main/java/com/seuprojeto/controller/demo/HelloController.java
@@ -1,12 +1,13 @@
 package com.seuprojeto.controller.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/test")
-    public String helloWorld() {
-        return "Hello, World!";
+    public String helloWorld(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Olá %s!", name);
     }
 }
